24703465a3937ff9bb682c7bcb12efa83440840a
Jason New <jasonn@Jasons-MacBook-Pro.local>
2021-02-05 11:00:35 +0200

    - Removed unnecessary checks on POST data - Re-added retrieving user based on email - WPCS updates

--- includes/classes/class-ct-db-registration.php
+++ includes/classes/class-ct-db-registration.php
@@ -58,14 +58,15 @@ if( ! class_exists( 'CT_DB_Registration' ) ) { // Don't initialise if there's al
 		public function return_login_registration_form( $atts, $content = '' ) {
 
 			$successful_registration = false;
-			if ( ! empty( $_POST ) && isset( $_POST['ctdb_user_email'] ) ) {
+			if ( ! empty( $_POST['ctdb_user_email'] ) ) {
 				$user_email = sanitize_email( wp_unslash( $_POST['ctdb_user_email'] ) );
+				$user = get_user_by( 'email', $user_email );
 				if ( $user ) {
 					$successful_registration = true;
 				}
 			}
 
-			if ( ! is_user_logged_in() && ! $successful_registration) {
+			if ( ! is_user_logged_in() && ! $successful_registration ) {
 
 				$message = '';
 				$class = '';
