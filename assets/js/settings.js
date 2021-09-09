deea15a095c0d152d117d295dcd0712544ced822
Ashish Ravi <contactashish13@gmail.com>
2021-09-09 10:30:09 +0530

    increase width to 300px

--- assets/js/settings.js
+++ assets/js/settings.js
@@ -4,7 +4,8 @@
     });
 
     function init(){
-        $('select[name="ctdb_user_settings[discussion_board_minimum_role][]"]').select2();
-        $('select[name="ctdb_user_settings[minimum_user_roles][]"]').select2();
+        $('select[name="ctdb_user_settings[discussion_board_minimum_role][]"], select[name="ctdb_user_settings[minimum_user_roles][]"]').select2({
+            width: '300px'
+        });
     }
 })(jQuery, config);
